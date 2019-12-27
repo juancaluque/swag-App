63d3d69686166f41c82b1d1b089f4a81622594f0
Jukess <juancaluque@hotmail.es>
2019-12-26 20:30:16 -0600

    lesson-01 done

---
+++ swag-App/swag-App/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  swag-App
+//
+//  Created by Juan Luque on 12/26/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
