1fa2f9d85e7618b90df298277f0e9381002cea2c
Jukess <juancaluque@hotmail.es>
2019-12-28 19:54:23 -0600

    lesson-01 done

---
+++ swag-App/swag-App/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  swag-App
+//
+//  Created by Juan Luque on 12/28/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    
+}
