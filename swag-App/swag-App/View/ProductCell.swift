0b5d962e9a2e8faf3588f9180335301a1ade5287
Jukess <juancaluque@hotmail.es>
2019-12-29 11:39:09 -0600

    lesson-02 done

---
+++ swag-App/swag-App/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  swag-App
+//
+//  Created by Juan Luque on 12/28/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
