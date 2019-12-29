2c308f368d3e66266ceaad46d6b50602a5638c7e
Jukess <juancaluque@hotmail.es>
2019-12-29 12:54:45 -0600

    lesson-05 done

---
+++ swag-App/swag-App/Controller/ProductsVC.swift
@@ -0,0 +1,43 @@
+//
+//  ProductsVC.swift
+//  swag-App
+//
+//  Created by Juan Luque on 12/29/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var productCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        productCollection.dataSource = self
+        productCollection.delegate = self
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+    }
+    
+}
